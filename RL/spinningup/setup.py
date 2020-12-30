e4b7b6a5f0a025bf068c7d73f55f68bdfc92cf13
ankdesh <a.s.deshwal@gmail.com>
2020-12-30 10:18:17 +0530

    VPG coding with Spinning up

--- RL/spinningup/setup.py
+++ RL/spinningup/setup.py
@@ -26,8 +26,7 @@ setup(
         'psutil',
         'scipy',
         'seaborn==0.8.1',
-        'tensorflow>=1.8.0,<2.0',
-        'torch==1.3.1',
+        'torch==1.4.0',
         'tqdm'
     ],
     description="Teaching tools for introducing people to deep RL.",
