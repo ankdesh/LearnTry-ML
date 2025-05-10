63aead6a7c87e0aec30c188c34c71ce4c15ba5c6
ankdesh <a.s.deshwal@gmail.com>
2025-05-10 18:57:47 +0530

    Add support for directly executing calc op

--- llm/a2a_adk/google_adk/agent.py
+++ llm/a2a_adk/google_adk/agent.py
@@ -13,5 +13,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-root_agent=CalculatorAgent() # Instantiate with CalculatorAgent
-    
+root_agent=CalculatorAgent()._build_agent() # Instantiate with CalculatorAgent
+
+
