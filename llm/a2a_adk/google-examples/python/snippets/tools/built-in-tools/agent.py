36b89ca305638a02c76dfc00d8cc1934c3bdbdcc
ankdesh <a.s.deshwal@gmail.com>
2025-05-11 17:07:37 +0530

    Some update to google adk examples

--- llm/a2a_adk/google-examples/python/snippets/tools/built-in-tools/agent.py
+++ llm/a2a_adk/google-examples/python/snippets/tools/built-in-tools/agent.py
@@ -1,18 +1,5 @@
-from common.server import A2AServer
-from common.types import AgentCard, AgentCapabilities, AgentSkill, MissingAPIKeyError
-from task_manager import AgentTaskManager
-from code_execute import CodeExecAgent 
-import click
-import os
-import logging
-from dotenv import load_dotenv
+from .code_execution import code_agent
 
-load_dotenv()
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
-root_agent=CodeExecAgent()._build_agent() # Instantiate with CalculatorAgent
+root_agent=code_agent # Instantiate with CalculatorAgent
 
 
---
+++ llm/a2a_adk/google-examples/python/snippets/tools/function-tools/agent.py
@@ -0,0 +1,4 @@
+from .file_processor import file_processor_agent
+
+root_agent=file_processor_agent
+
